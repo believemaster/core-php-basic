feb9b0a7e743f0b197bd5731f944712990198a4a
believemaster <believemasters@gmail.com>
2022-04-02 14:24:26 +0530

    validate the page number using filter var

--- Blog with PDO/classes/Paginator.php
+++ Blog with PDO/classes/Paginator.php
@@ -29,6 +29,14 @@
     */
     public function __construct($page, $records_per_page) {
       $this->limit = $records_per_page;
+
+      $page = filter_var($page, FILTER_VALIDATE_INT, [
+        'options' => [
+          'default' => 1,
+          'min_range' => 1
+        ]
+      ]);
+      
       $this->offset = $records_per_page * ($page - 1);
     }
   }
