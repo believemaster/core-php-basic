82b07d8210245cf419b579cdd0f3c81246cd7c6e
believemaster <believemasters@gmail.com>
2022-04-02 11:07:48 +0530

    Created admin index and loaded classes relative to root directory

---
+++ Blog with PDO/admin/index.php
@@ -0,0 +1,35 @@
+<?php
+
+require('../include/init.php');
+
+$conn = require('../include/db.php');
+
+$articles = Article::getAll($conn);
+
+?>
+
+<?php include("../include/header.php") ?>
+
+<?php if(Auth::isLoggedIn()): ?>
+  <p>You are logged in. <a href="logout.php">Logout</a> </p>
+  <p><a href="newarticle.php">New Article</a></p>
+<?php else: ?>
+  <p>You are not logged in. <a href="login.php">Login</a> </p>
+<?php endif; ?>
+
+<?php if (empty($articles)): ?>
+  <p>No Articles Found.</p>
+<?php else: ?>
+  <ul>
+    <?php foreach ($articles as $article): ?>
+      <li>
+        <article class="">
+          <h2><a href="article.php?id=<?= $article['id']; ?>"><?= htmlspecialchars($article['title']); ?></a> </h2>
+          <p><?= htmlspecialchars($article['content']); ?></p>
+        </article>
+      </li>
+    <?php endforeach; ?>
+  </ul>
+<?php endif; ?>
+
+<?php include('../include/footer.php'); ?>
