eecb13e84f285c96846edb370662d9386a3f3fe2
believemaster <believemasters@gmail.com>
2022-06-04 08:50:56 +0530

    Using jquery instead of JS

--- JS in PHP/script.js
+++ JS in PHP/script.js
@@ -1,5 +1,11 @@
+/********* Vanilla JS ************/
+
 function sayHello() {
   alert("Hello");
 }
 
 document.getElementById("click").addEventListener("click", sayHello);
+
+
+/********* Using JQuery *********/
+$('#click').on('click', sayHello());
